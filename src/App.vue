0e950a0c5744618aa7fd7dc2b771c1e484339d86
grinzing <grinzing35@gmail.com>
2020-05-30 17:22:37 +0900

    janken勝敗なし完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="isEveryoneSelect" class=result>
+      <h2 v-for="user in $whim.users" :key="user.id">
+        <img
+          :src="require('@/assets/' + $whim.state[user.id] + '.png')"
+          width="150"
+          height="150"
+        />
+        <h2>{{user.name}}</h2>
+      </h2>
+    </div>
+    <div v-else-if="$whim.state[$whim.accessUser.id]">
+      <h2>
+        {{ $whim.state[$whim.accessUser.id] }}を選択済みです。
+      </h2>
+    </div>
+    <div v-else>
+      <h2>
+        選択してください！
+      </h2>
+      <div>
+        <img
+          src="@/assets/rock.png"
+          width="150"
+          height="150"
+          @click="select('rock')"
+        />
+        <img
+          src="@/assets/scissors.png"
+          width="150"
+          height="150"
+          @click="select('scissors')"
+        />
+        <img
+          src="@/assets/paper.png"
+          width="150"
+          height="150"
+          @click="select('paper')"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    select(hand) {
+      // ここに処理を書いていく
+      console.log('selectされた！')
+      this.$whim.assignState({
+        [this.$whim.accessUser.id]: hand
+      })
+    }
+  },
+  computed: {
+    isEveryoneSelect(){
+      let result = true
+      for (let i = 0; i<this.$whim.users.length; i++){
+        if(!this.$whim.state[this.$whim.users[i].id]){
+          result = false
+        }
+      }
+      return result
+    }
   }
 }
 </script>
@@ -24,5 +77,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.result {
+  display: flex;
+  justify-content: center;
 }
 </style>
